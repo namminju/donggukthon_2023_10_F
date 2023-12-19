c141c626153472c065b4d904333dd58fe2857e37
namminju <nampink2300@gmail.com>
2023-12-20 02:19:14 +0900

    feat: 회원가입페이지 구성중

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Start from '../src/Pages/Start';
 import Login from '../src/Pages/Login';
 import My_Igloo from '../src/Pages/My_Igloo';
+import JoinMembership from '../src/Pages/JoinMembership';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Start />} />
         <Route path="/login" element={<Login />} />
         <Route path="/myigloo" element={<My_Igloo />} />
+        <Route path="/joinmembership" element={<JoinMembership/>} />
       </Routes>
     </Router>
   );
